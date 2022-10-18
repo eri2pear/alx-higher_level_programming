00aaaebaa9a8226cd426a8bf1bd72f91cb020088
Patricia Atrisia Rukud'de <eri2pear@gmail.com>
2022-10-18 13:03:37 -0700

    Everything is an object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,5 +6,3 @@ class LockedClass():
     """ prevents creating new instance attributes, \
         except if the new instance is called first_name"""
 	__slots__ = ["first_name"]
-	def __init__(self, first_name=''):
-	        self.first_name = first_name
