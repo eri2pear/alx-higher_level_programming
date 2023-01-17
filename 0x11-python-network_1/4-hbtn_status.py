229ce2550cbfc7826c03061652748baaba77f613
Patricia Atrisia Rukud'de <eri2pear@gmail.com>
2023-01-17 23:03:48 +0300

    status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,6 +7,7 @@
 import requests
 
 if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:\n\t- type: {}\n\t- content: {}"
-          .format(type(r.text), r.text))
+    r = requests.get('https://intranet.hbtn.io/status').text
+    print("Body response:")
+    print("\t- type: {}".format(type(r)))
+    print("\tt- content: {}".format(r:))
